510cb0698711a0e7d2c68f62c2ee61f9a0a6e7f1
Pimped Toloy <59160501@go.buu.ac.th>
2019-10-12 11:21:13 +0700

    Pokemon mongo V.2

--- server.js
+++ server.js
@@ -14,39 +14,73 @@ const client = new MongoClient(MongoURL,options)
 
 app.use(express.json())
 
-app.post('/pokemons',(req, res)=>{
-    client.connect(connected)
-    function connected(err,client){
-
-    }
-
-
-    client.connect((err,client)=> {
-        if(err){
-            console.log(err)
-            res.status(500).send({error:err})
-            return 
-        }
-        let db = client.db(database)
-        db.collection(collection).insertOne({name:"test"}, (err, r)=>{
-            res.status(201).send({message:'Create pokemon successfully'})
-            return
-        })
+app.post('/pokemons', async (req, res)=>{
+    let con = await client.connect().catch(err => {
+        console.log("Error orccurred when try to connect Mongo")
+        console.log(err)
+        res.status(500).send({error:err})
+        return
     })
+
+    let db = con.db(database)
+    let result = await db.collection(collection).insertOne({name:"Pikachu"}).catch(err => {
+        console.log("Error orccurred when try to insert pokemon")
+        console.log(err)
+        res.status(500).send({error:err})
+        return
+    })
+
+    res.status(201).send({message:'Create pokemon successfully'})
+
+    //---------------------------------------------------------------------------------
+    // client.connect((err,client)=> {
+    //     if(err){
+    //         console.log(err)
+    //         res.status(500).send({error:err})
+    //         return 
+    //     }
+    //     let db = client.db(database)
+    //     db.collection(collection).insertOne({name:"test"}, (err, r)=>{
+    //         res.status(201).send({message:'Create pokemon successfully'})
+    //         return
+    //     })
+    // })
+    //---------------------------------------------------------------------------------
+
+    // res.status(201).send({message:'Finished too early'})
 })
 
-app.get('/airbnb/list-reviews',(req, res)=>{
-    client.connect((err,client)=> {
-        if(err){
-            console.log(err)
-            res.status(500).send({error:err})
-            return 
-        }
-        let db = client.db("sample_airbnb")
-        db.collection("listingsAndReviews").find( {} ).limit(10).toArray((err,r)=>{
-            res.send(r)
-        })
+app.get('/airbnb/list-reviews', async (req, res)=>{
+
+
+    let con = await client.connect().catch(err => {
+        console.log("Error orccurred when try to connect Mongo")
+        console.log(err)
+        res.status(500).send({error:err})
+        return
     })
+
+    let db = con.db("sample_airbnb")
+    let result = await db.collection("listingsAndReviews").find( {} ).limit(10).toArray().catch(err => {
+        console.log("Error orccurred when try to insert pokemon")
+        console.log(err)
+        res.status(500).send({error:err})
+        return
+    })
+
+    res.send(result)
+
+    // client.connect((err,client)=> {
+    //     if(err){
+    //         console.log(err)
+    //         res.status(500).send({error:err})
+    //         return 
+    //     }
+    //     let db = client.db("sample_airbnb")
+    //     db.collection("listingsAndReviews").find( {} ).limit(10).toArray((err,r)=>{
+    //         res.send(r)
+    //     })
+    // })
 })
 
 
